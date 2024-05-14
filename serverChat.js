6d579277da1ba87c47e25dc17132710972650255
CodeChef <joetomsim@gmail.com>
2024-05-14 13:09:49 -0500

    chat work

--- serverChat.js
+++ serverChat.js
@@ -1,19 +1,5 @@
 const express = require('express');
-const path = require('path');
 const app = express();
-const exphbs = require('express-handlebars');
-const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const PORT = process.env.PORT || 3001;
-
-const routes = require('./controllers');
-
-
-const sequelize = require('./config/connections');
-const helpers = require('./utils/helpers');
-
-// app.use(express.static(path.join(__dirname, 'public')));
-// app.use(require('./controllers/api/routes'));
 
 const apiRoutes = require("./controllers")
 
@@ -29,12 +15,17 @@ const io = socketIo(server);
 const chatRoutes = require('./controllers/chat/index')(io);
 const mount = require("./services/socketio")
 
+
+const PORT = process.env.PORT || 3001;
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.use("/api/chat", chatRoutes)
 app.use('/', apiRoutes);
+
+
 
 /**
  * This calls a function in services/socketio.js that basically
