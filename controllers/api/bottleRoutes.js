01ad066f06e4a44c61fe2d27a32b28a5ed03928b
kyle butcher <kylembutcher97@gmail.com>
2024-05-15 11:52:11 -0500

    got another route set up. i think accolade route will be good to go once i finish filling this out

--- controllers/api/bottleRoutes.js
+++ controllers/api/bottleRoutes.js
@@ -16,3 +16,35 @@ router.get('/:id', async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+// POST create a new journal entry for the whiskey product
+
+router.post('/', async (req, res) => {
+  try {
+    const bottleData = await User.create(req.body);
+    res.status(200).json(bottleData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+  });
+
+// PUT update a journal entry
+router.put('/:id', async (req, res) => {
+  try {
+    const bottleData = await User.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+      individualHooks: true
+    });
+    if (!bottleData[0]) {
+      res.status(404).json({ message: 'No user with this id!' });
+      return;
+    }
+    res.status(200).json(bottleData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
