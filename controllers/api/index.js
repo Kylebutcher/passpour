4fb2c116274f2a116680e60eb48c3c10fec83bc0
CodeChef <joetomsim@gmail.com>
2024-05-17 10:21:41 -0500

    its the next day, idk

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -7,15 +7,13 @@ const bottles = require('./bottleRoutes');
 
 const testRoutes = require('./test.controller');
 
-
-
 //Database / Models
 router.use('/users', users);
 router.use('/accolades', accolades);
 router.use('/bottles', bottles);
 
 //Socket Testing
-router.use('/test', testRoutes)
+// router.use('/test', testRoutes)
 
 
 module.exports = router;  
