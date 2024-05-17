be7b77cd8adb97901236b06c61138a775b21b175
kyle butcher <kylembutcher97@gmail.com>
2024-05-17 01:32:20 -0500

    yuh

--- controllers/api/accoladeRoutes.js
+++ controllers/api/accoladeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Accolade, UserAccolade, User } = require('../../models');
+const { Accolade } = require('../../models/Accolade');
 
 
 // GET all Accolades
@@ -30,23 +30,6 @@ router.get('/:id', async (req, res) => {
 // ASK KATY
 // update methoud for sequlize (find 1 + update)
 // POST give accolade to user
-// for the comments above.
-router.put('/:id', async (req, res) => {
-// const accolade = Accolade.findByPk(req.params.id)
-  const updateAccolade = await User.update()
-})
-
-router.post('/:id', async (req, res) => {
-  try{
-    const addAccolade = await UserAccolade.create({ user_id: user, 
-    accolade_id:req.params.id});
-  const user = User.findByPk(req.session.user_id);
-      res.status(200).json(addAccolade);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
-
 router.post('/', async (req, res) => {
   try {
     const accoladeData = await User.create(req.body);
