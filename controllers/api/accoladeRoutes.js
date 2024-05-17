4ce41776178aeb17de138cbefb5d4ce78599bc5c
Brittany Jungclaus <novalancebrittany@gmail.com>
2024-05-17 00:45:55 -0500

    fleshed out more API files

--- controllers/api/accoladeRoutes.js
+++ controllers/api/accoladeRoutes.js
@@ -3,7 +3,7 @@ const { Accolade } = require('../../models/Accolade');
 
 
 // GET all Accolades
-router.get('/', async (req, res) => {
+router.get('/accolades', async (req, res) => {
   Accolade.findAll().then((AccoladeData) => {
     res.json(AccoladeData);
   });
@@ -12,7 +12,7 @@ router.get('/', async (req, res) => {
 
 
 // GET a specific accolade
-router.get('/:id', async (req, res) => {
+router.get('/accolade/:id', async (req, res) => {
   try {
     const accoladeData = await User.findByPk(req.params.id);
     if (!accoladeData) {
