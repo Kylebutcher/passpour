daa0b7ed7ce858d92f7cb06532e2379f8f6e4917
Brittany Jungclaus <164208757+NovaLanceBrittany@users.noreply.github.com>
2024-05-19 05:42:50 -0500

    JS for explore and showcase 90% finished, modal 70% finished, was working on certain display bugs from a bootstrap error for along time, but got passed it

--- public/assets/js/explore.js
+++ public/assets/js/explore.js
@@ -1,7 +1,7 @@
 
 const bottleCards = document.querySelector('#bottle-cards'); // Needs Attention
 
-const cubbies = document.querySelector('.cubbies')
+const cubbies = document.querySelectorAll('.cubbies')
 
 
 
@@ -42,74 +42,69 @@ function printResults(data){
 
 
 
-// function printBottle(bottleCard, i) {
+function printBottle(bottleCard, i) {
   
-//   for (let i = 0; i < cubbies.length; i++) {
-//     console.log(cubbies[i]);
+  for (let i = 0; i < cubbies.length; i++) {
+    console.log(cubbies[i]);
     
-//     const wnameEl = document.createElement('h3');
-//     wnameEl.textContent = bottleCard.name;
+    const wnameEl = document.createElement('h3');
+    wnameEl.textContent = bottleCard.name;
     
-//     // The Styleing of the wnameEl only
-//     wnameEl.setAttribute('style', 'font-size: 16px; font-weight: bold; text-shadow: black 0 0 .3rem; color: var(--whiskey-orange)')
-    
-    
-//     const wTypeEl = document.createElement('p');
-//     wTypeEl.textContent = bottleCard.whiskey_type;
-    
-//     // const orderEl = document.createElement('p');
-//     // orderEl.textContent = bottleCard.order;
-    
-//     // const tasteNotesEl = document.createElement('p');
-//     // tasteNotesEl.textContent = bottleCard.taste_notes;
-    
-//     // The Styleing of the wType, orderEl, tasteNotesEl only
-//     wnameEl.setAttribute('style', 'font-size: 12px; font-weight: bold; text-shadow: black 0 0 .3rem; color: var(--whiskey-orange)')
+    // The Styleing of the wnameEl only
+    wnameEl.setAttribute('style', 'font-size: 16px; font-weight: bold; text-shadow: black 0 0 .3rem; color: var(--whiskey-orange)')
     
     
-//     const card = document.createElement('div');
-//     card.classList.add('col-3')
+    const wTypeEl = document.createElement('p');
+    wTypeEl.textContent = bottleCard.whiskey_type;
     
-//     // Styleing for the Card container
-//     card.setAttribute('style', 'line-height: .5rem; padding: 5px; text-shadow: black 0 0 .3rem;')
+    const orderEl = document.createElement('p');
+    // orderEl.textContent = bottleCard.order;
     
-//     card.appendChild(wnameEl) 
-//     card.appendChild(wTypeEl)
-//     // card.appendChild(orderEl)
-//     // card.textContent = 'hello';
-//     // card.appendChild(tasteNotesEl)
+    const tasteNotesEl = document.createElement('p');
+    // tasteNotesEl.textContent = bottleCard.taste_notes;
     
-//     cubbies.appendChild(card)
-    
-//     // console.log(cubbies[0]);
-//   }
-// }
 
-// printBottle("printbottle call line 88", 2);
+
+    // The Styleing of the wType, orderEl, tasteNotesEl only
+    orderEl.setAttribute('style', 'font-size: 12px; font-weight: bold; text-shadow: black 0 0 .3rem; color: var(--whiskey-orange)')
+
+    wTypeEl.setAttribute('style', 'font-size: 12px; font-weight: bold; text-shadow: black 0 0 .3rem; color: var(--whiskey-orange)')
+
+    tasteNotesEl.setAttribute('style', 'font-size: 12px; font-weight: bold; text-shadow: black 0 0 .3rem; color: var(--whiskey-orange)')
+
+    
+    
+    const card = document.createElement('div');
+    card.classList.add('col-3')
+    
+    // Styleing for the Card container
+    card.setAttribute('style', 'line-height: .5rem; padding: 5px; text-shadow: black 0 0 .3rem;')
+    
+    card.appendChild(wnameEl) 
+    card.appendChild(wTypeEl)
+    // card.appendChild(orderEl)
+    // card.textContent = 'hello';
+    // card.appendChild(tasteNotesEl)
+    
+    cubbies.appendChild(card)
+    
+    // console.log(cubbies[0]);
+  }
+  printBottle("printbottle call line 88", 2);
+}
+
 
 
 
 // Modal Script
-var modal = document.querySelector("#bottlemodal");
-var button = document.querySelector(".showcasebtn");
-var closeButton = document.querySelector(".btn-close");
+const submitBtn = $('#submitBtn');
+const closeMdl = $('#formModal');
 
-// When the user clicks on the button it will open the modal
-button.addEventListener('click', function (e) {
-  e.preventDefault();
-
-  modal.style.display = "block";
-  console.log("modal click")
+// Event listener for submitting task
+submitBtn.on('click', function(){
+  $(closeMdl).modal('toggle');
 })
 
-
-// When the user clicks on the (X) button to exit the modal
-closeButton.addEventListener('click', function (e) {
-  e.preventDefault();
-
-  modal.sytle.display = "none";
-  console.log("modal closed")
-})
 
 
 
