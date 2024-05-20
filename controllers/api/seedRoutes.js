9d84c4d94ade4da0c2d249a456c76594dd4bccab
Brittany Jungclaus <164208757+NovaLanceBrittany@users.noreply.github.com>
2024-05-20 17:17:21 -0500

    seeded Data for Render Host is now complete!

--- controllers/api/seedRoutes.js
+++ controllers/api/seedRoutes.js
@@ -6,8 +6,17 @@ const userData = require("../../db/seeds/user-data.json")
 
 router.post("/", async (req, res) => {
 
-  // Tyler and I tried this adjustment to line 10, then rememebered that I needed to push the current code to make it available to Gary.
-  await User.destroy({ truncate : true, cascade: false })
+  // Tyler and I tried this adjustment to line 10-19, added then/catch block, and some syntax was adjusted. Pleaes take a look at the file: models>index.js on line 40 there was a left over SET and we placed only CASCADE. 
+  await User.destroy({
+    truncate: true,
+    cascade: true
+  })
+  .then(() => {
+    console.log('Table truncated successfully.');
+  })
+  .catch((error) => {
+    console.error('Error truncating table:', error);
+  });
 
   const usersUpdate = userData.map(user => {
     const { id, ...update } = user
